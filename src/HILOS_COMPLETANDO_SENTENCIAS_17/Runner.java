882369b6bcc043177b053be8af73a8d698404bd0
miguelroy01lop@gmail.com <miguel56mrypz@gmail.com>
2023-05-25 01:50:17 -0600

    Programa subido a GITHUB

--- src/HILOS_COMPLETANDO_SENTENCIAS_17/Runner.java
+++ src/HILOS_COMPLETANDO_SENTENCIAS_17/Runner.java
@@ -1,14 +1,13 @@
 package HILOS_COMPLETANDO_SENTENCIAS_17;
 
 public class Runner {
-    public static void escribir(String palabra, int velocidad){
+    public static void escribir(String palabra, int velocidad) {
         char[] caracteres = palabra.toCharArray();
         for (int i=0; i<caracteres.length; i++){
             System.out.print(caracteres[i]);
             try{
                 Thread.sleep(velocidad);
             }
-
             catch (Exception e){
                 System.out.println(e);
             }
